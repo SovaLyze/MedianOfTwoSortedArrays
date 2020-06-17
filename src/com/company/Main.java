d26e92b7eee55add4e0966fdb3bb5b114a665366
Sova <yuralakey22@gmail.com>
2020-06-17 13:13:51 +0200

    Median of two sorted Arrays

---
+++ src/com/company/Main.java
@@ -0,0 +1,72 @@
+package com.company;
+
+public class Main {
+
+    static double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+
+        if(m > n) {
+            int[] temp = A; A = B; B = temp;
+            int tmp = m; m = n; n = tmp;
+        }
+
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j - 1] > A[i]) {
+                iMin = i + 1; // i is too small
+            }
+            else if (i > iMin && A[i - 1] > B[j]) {
+                iMax = i - 1; // i is too bid
+            }
+            else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                }
+                else if (j == 0) {
+                    maxLeft = A[i - 1];
+                }
+                else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ( (m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                }
+                else if (j == n) {
+                    minRight = A[i];
+                }
+                else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+
+        int nums1[] = {1, 12, 15, 26, 38};
+        int nums2[] = {17, 30, 45};
+
+        System.out.println("Median is: " + findMedianSortedArrays(nums1, nums2));
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1 == n2) {
+            System.out.println("Median is: " + findMedianSortedArrays(nums1, nums2));
+        }
+        else {
+            System.out.println("Arrays are of unequal size! ");
+        }
+    }
+}
